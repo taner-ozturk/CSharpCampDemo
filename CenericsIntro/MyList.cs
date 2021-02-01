c960d805f1812e53ca856d6af7f26a00fea1fb8d
Taner Öztürk <taner1ozturk@gmail.com>
2021-02-01 23:17:21 +0300

    CenerricsIntro and Collections Added

---
+++ CenericsIntro/MyList.cs
@@ -0,0 +1,40 @@
+﻿namespace CenericsIntro
+{
+    public class MyList<T>
+    {
+
+        T[] items;
+        //constructor
+        
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        
+
+        public void Add(T item)
+        {
+
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+                
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+
+        public int Length => items.Length;
+        public T[] Items
+        {
+            get
+            {
+                return items;
+            }
+        }
+    }
+}
---
+++ CenericsIntro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Taner");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Kerem");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+
+            {
+                Console.WriteLine(isim);
+
+            }
+
+        }
+    }
+}
